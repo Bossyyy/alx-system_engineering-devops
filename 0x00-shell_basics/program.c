0d45202b1914557cf592c4d9588fea0368dd5e08
Bossyyy <emmanuelapeagyei2@gmail.com>
2023-07-06 16:17:24 -0700

    html

--- 0x00-shell_basics/program.c
+++ 0x00-shell_basics/program.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void __attribute__((section(".gnu.warning"))) strip_warning() {
+    system("strip iamafile");
+}
+
 int main() {
     printf("This is a C program.\n");
     return 0;
